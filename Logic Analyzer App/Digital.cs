85fa3ed9b2021ccb502a3ef697c0170c8467414a
Harley Ovell <hovell3@gatech.edu>
2020-04-19 14:45:41 -0400

    Instead of displaying values, the textbox background color changes when it detects a digital 1

--- Logic Analyzer App/Digital.cs
+++ Logic Analyzer App/Digital.cs
@@ -19,27 +19,21 @@ namespace Logic_Analyzer_App
         static SerialPort Port;
         public bool DigOpen { get; set; }
         byte[] bits = new byte[1] { 0 }; //string used for mbed communication
-        static bool _readon;
-        static int pinNum1;
-        static int pinNum2;
-        static int pinNum3;
-        static int pinNum4;
-        static int pinNum5;
-        static string mbed;
-        static string text1;
-        //Thread readThread = new Thread(Read);
-        public delegate void DisplaySerial(string mbedstuff);
+
+        public delegate void DisplaySerial(int mbedstuff);
         public DisplaySerial mbedSerial;
 
         public Digital(string ComPort, bool IsOpen)
         {
 
             InitializeComponent();
+            mbedSerial = new DisplaySerial(SerialtoTextMethod);
 
             if (!IsOpen & !string.IsNullOrEmpty(ComPort))
             {
                 Port = new SerialPort(ComPort, 9600, Parity.None, 8, StopBits.One);
                 Port.Open();
+                Port.DataReceived += new SerialDataReceivedEventHandler(YouveGotCereal);
                 IsOpen = true;
                 
             }
@@ -64,58 +58,40 @@ namespace Logic_Analyzer_App
 
         }
 
-        private void Pin1Combo_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            //string activePin1 = this.Pin1Combo.GetItemText(this.Pin1Combo.SelectedItem);
-            int pinNUM1 = (this.Pin1Combo.SelectedIndex);
-            bits[0] = 0x30;
-        }
 
         private void Digital_FormClosing(object sender, FormClosingEventArgs e)
         {
             DigOpen = false;
+            bits[0] = 0;
+            Port.Write(bits, 0, 1);
             Port.Close();
-        }
-
-        private void Pin2Combo_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            int pinNUM2 = (this.Pin2Combo.SelectedIndex);
-            bits[0] = 0x30;
-        }
-
-        private void Pin3Combo_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            int pinNUM3 = (this.Pin3Combo.SelectedIndex);
-            bits[0] = 0x30;
-        }
-
-        private void Pin4Combo_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            int pinNUM4 = (this.Pin4Combo.SelectedIndex);
-            bits[0] = 0x30;
-        }
-
-        private void Pin5Combo_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            int pinNUM5 = (this.Pin5Combo.SelectedIndex);
-            bits[0] = 0x30;
         }
 
         private void StartButton_Click(object sender, EventArgs e)
         {
-            bits[2] = bits[0] = 0x30; //first and third bit in array tell mbed to read data and to use digital
+            bits[0] += 32; //first and third bit in array tell mbed to read data and to use digital
+            if (Pin21_Select.Checked) { bits[0] += 16; }
+            if (Pin22_Select.Checked) { bits[0] += 8; } 
+            if (Pin23_Select.Checked) { bits[0] += 4; }
+            if (Pin24_Select.Checked) { bits[0] += 2; }
+            if (Pin25_Select.Checked) { bits[0] += 1; }
             //_readon = true;//serial communication ready, used in Read thread
             //readThread.Start();//starts thread
             //Port.Write(ConvertStringArrayToString(bits));//tells mbed to start
-            Port.Write("00100000");
+            Port.Write(bits, 0, 1);
+            bits[0] = 0; //Clears the bits
         }
 
         private void StopButton_Click(object sender, EventArgs e)
         {
-            bits[0] = bits[1] = bits[2] = bits[3] = bits[4] = bits[5] = bits[6] = bits[7] = 0; //sets the R bit to 0 to stop digital collection
-            //_readon = false; //tells the Read thead to stop
-            //Port.Write(ConvertStringArrayToString(bits)); //tells mbed to stop
-            Port.Write("0x00");
+            bits[0] = 0; //sets the R bit to 0 to stop digital collection
+            Pin1Out.BackColor = SystemColors.Control;
+            Pin2Out.BackColor = SystemColors.Control;
+            Pin3Out.BackColor = SystemColors.Control;
+            Pin4Out.BackColor = SystemColors.Control;
+            Pin5Out.BackColor = SystemColors.Control;
+            Port.Write(bits, 0, 1);
+
 
         }
         private void Pin1Out_TextChanged(object sender, EventArgs e)
@@ -125,38 +101,86 @@ namespace Logic_Analyzer_App
 
         public void SerialtoTextMethod(int thestring)
         {
-            Pin1Out.Text = thestring.ToString();
+            thestring -= 32;
+            if (thestring >= 16)
+            {
+                Pin1Out.BackColor = SystemColors.Highlight;
+                thestring -= 16;
+            }
+            else if (thestring >= 8)
+            {
+                Pin2Out.BackColor = SystemColors.Highlight;
+                thestring -= 8;
+            }
+            else if (thestring >= 4)
+            {
+                Pin3Out.BackColor = SystemColors.Highlight;
+                thestring -= 4;
+            }
+            else if (thestring >= 2)
+            {
+                Pin4Out.BackColor = SystemColors.Highlight;
+                thestring -= 2;
+            }
+            else if (thestring > 0)
+            {
+                Pin5Out.BackColor = SystemColors.Highlight;
+                thestring -= 1;
+            }
+            else
+            {
+                Pin1Out.BackColor = SystemColors.Control;
+                Pin2Out.BackColor = SystemColors.Control;
+                Pin3Out.BackColor = SystemColors.Control;
+                Pin4Out.BackColor = SystemColors.Control;
+                Pin5Out.BackColor = SystemColors.Control;
+            }
+            
         }
 
         private void YouveGotCereal(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
         {
+            
             int mbedstuff = Port.ReadChar();
             Invoke(mbedSerial, mbedstuff);
         }
 
-        //public static void Read() // Read thread is for serial communication with the mbed
-        //{
-        //    while (_readon)
-        //    {
-        //        try
-        //        {
-        //            mbed = Port.ReadExisting();
 
-        //        }
-        //        catch { }
-        //    }
-        //}
-
-        static string ConvertStringArrayToString(string[] array) //converts the string array to one string for serial communication
-        {   //this was found online, thought it would be easy to use but have not verified its output
-            // Concatenate all the elements into a StringBuilder.
-            StringBuilder builder = new StringBuilder();
-            foreach (string value in array)
-            {
-                builder.Append(value);
-                builder.Append('.');
+        private void Pin21_Select_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Pin21_Select.Checked)
+            { 
+                bits[0] += 16;
             }
-            return builder.ToString();
+            
+        }
+
+
+        private void Pin22_Select_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Pin22_Select.Checked)
+            {
+                bits[0] += 8;
+            }
+                
+        }
+
+        private void Pin23_Select_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Pin23_Select.Checked)
+            { bits[0] += 4; }
+        }
+        private void Pin24_Select_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Pin24_Select.Checked)
+            { bits[0] += 2; }
+                
+        }
+
+        private void Pin25_Select_CheckedChanged(object sender, EventArgs e)
+        {
+            if (Pin25_Select.Checked)
+            { bits[0] += 1; }
         }
     }
 }
